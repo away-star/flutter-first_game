318824235a05f155f3a3bb4167bd5435e7d686bc
xingxing2064989403 <2064989403@qq.com>
2023-03-16 23:40:32 +0800

    基本架构ok！

---
+++ lib/game.dart
@@ -0,0 +1,221 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  final _inputController = StreamController.broadcast();
+  final _scoreController = StreamController.broadcast();
+
+  //一次改进代码(使用StreamController的监听)
+  // num score = 0;
+  //
+  // @override
+  // void initState() {
+  //   _scoreController.stream.listen((score) {
+  //     setState(() {
+  //       this.score += score;
+  //     });
+  //   });
+  //
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //二次改进代码
+        title: Center(
+          child: StreamBuilder(
+              stream: _scoreController.stream.transform(TallyTransformer()),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text("Score: ${snapshot.data}");
+                }
+                return const Text("Score: 0");
+              }),
+        ),
+
+        //一次改进代码(使用StreamController的监听)
+        //title: Center(child: Text("Score: $score")),
+        //初始代码
+        // title: Center(
+        //   child: StreamBuilder(
+        //       stream: _scoreController.stream,
+        //       builder: (context, snapshot) {
+        //         if (snapshot.hasData&&snapshot.data!=null) {
+        //           score+=snapshot.data;
+        //         }
+        //         return Text("Score: $score");
+        //       }),
+        // ),
+      ),
+      body: Stack(children: [
+        //此处使用...将list展开成为一个个child widget
+        ...List.generate(
+            2,
+            (index) => Puzzle(
+                  inputStream: _inputController.stream,
+                  scoreStream: _scoreController,
+                )),
+        // Puzzle(inputStream: _controller.stream,),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: KeyPad(controller: _inputController)),
+      ]),
+    );
+  }
+}
+
+class TallyTransformer implements StreamTransformer {
+  num sum = 0;
+  StreamController _controller = StreamController();
+
+  @override
+  Stream bind(Stream stream) {
+    stream.listen((event) {
+      sum += event;
+      _controller.add(sum);
+    });
+    return _controller.stream;
+  }
+
+  @override
+  StreamTransformer<RS, RT> cast<RS, RT>() => StreamTransformer.castFrom(this);
+}
+
+class KeyPad extends StatelessWidget {
+  // const KeyPad({Key? key}) : super(key: key);
+
+  //将上面的_controller传给这个widget
+  final StreamController controller;
+
+  const KeyPad({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: const EdgeInsets.all(0.0),
+      //让它不占满整个屏幕
+      shrinkWrap: true,
+      crossAxisCount: 3,
+      childAspectRatio: 2 / 1,
+      children: List.generate(9, (index) {
+        return ElevatedButton(
+          //调整按键的颜色
+          style: ButtonStyle(
+            //调整按钮颜色
+            backgroundColor:
+                MaterialStateProperty.all(Colors.primaries[index][200]),
+            //让按钮的圆角消失
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0))),
+          ),
+          onPressed: () {
+            controller.add(index + 1);
+          },
+          child: Text(
+            'Item ${index + 1}',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class Puzzle extends StatefulWidget {
+  //const Puzzle(this.inputStream,{Key? key}) : super(key: key);
+
+  final inputStream;
+  final scoreStream;
+
+  const Puzzle(
+      {super.key, required this.inputStream, required this.scoreStream});
+
+  @override
+  State<Puzzle> createState() => _PuzzleState();
+}
+
+class _PuzzleState extends State<Puzzle> with SingleTickerProviderStateMixin {
+  late int a, b;
+
+  late Color color;
+
+  late double x;
+
+  late AnimationController _controller;
+  late Duration duration;
+
+  @override
+  void initState() {
+    //对动画controller进行初始化
+    _controller = AnimationController(
+      vsync: this,
+    ); //..repeat(reverse: true);
+    //先初始化_controller再对里面的duration进行初始化
+    reset(Random().nextDouble() * 0.2);
+
+    //监听动画播放结束的事件
+    _controller.addStatusListener((status) {
+      // print(widget.inputStream);
+      if (status == AnimationStatus.completed) {
+        reset();
+        //未答对 添加-3事件
+        widget.scoreStream.add(-3);
+      }
+    });
+
+    //监听用户输入的stream
+    widget.inputStream.listen((input) {
+      if (input == a + b) {
+        reset();
+        //答对了 添加5事件
+        widget.scoreStream.add(5);
+      }
+    });
+    super.initState();
+  }
+
+  //采用参数默认值 []
+  void reset([from = 0.0]) {
+    a = Random().nextInt(5) + 1;
+    b = Random().nextInt(5);
+    x = Random().nextDouble() * 300;
+    _controller.duration = Duration(milliseconds: Random().nextInt(800) + 4000);
+    color = Colors.primaries[Random().nextInt(Colors.primaries.length)][200]!;
+    _controller.forward(from: from);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, Widget? child) {
+        return Positioned(
+          //此处减去100是为了让动画的位置自然一点（不会自动消失）
+          top: MediaQuery.of(context).size.height / 1.3 * _controller.value -
+              100,
+          left: x,
+          child: Container(
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.6),
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.blue[300]!),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text("$a + $b", style: const TextStyle(fontSize: 24)),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/game.dart';
+
+void main() => runApp(ChatApp());
+
+class ChatApp extends StatelessWidget {
+ ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //把右上角的debug去掉
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter chat !',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Game(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //让这段文字居中
+        centerTitle: true,
+        title: const Text(
+          'flutter chat !',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (_, int index) => _messages[index],
+            ),
+          ),
+          Divider(height: 1.0),
+          //解释一下下面这段代码在干嘛
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.isNotEmpty;
+                  });
+                },
+                onSubmitted: _isComposing ? _handleSubmitted : null,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Type a message',
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: _isComposing
+                  ? () => _handleSubmitted(_textController.text)
+                  : null,
+            ),
+            IconButton(
+              icon: Icon(Icons.mic),
+              onPressed: _handleVoiceInput,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+      ChatMessage message = ChatMessage(
+        text: text,
+        isMe: true,
+      );
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleVoiceInput() {
+    // implement voice input
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final bool isMe;
+
+  ChatMessage({required this.text, required this.isMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment:
+            isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          !isMe
+              ? CircleAvatar(
+                  child: Text('Other'),
+                )
+              : SizedBox(),
+          Container(
+            margin: EdgeInsets.only(
+              left: isMe ? 50.0 : 0.0,
+              right: isMe ? 0.0 : 50.0,
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+            decoration: BoxDecoration(
+              color: isMe
+                  ? Theme.of(context).primaryColor
+                  : Theme.of(context).cardColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0),
+                bottomLeft: isMe ? Radius.circular(16.0) : Radius.zero,
+                bottomRight: isMe ? Radius.zero : Radius.circular(16.0),
+              ),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+          isMe
+              ? CircleAvatar(
+                  child: Text('Me'),
+                )
+              : SizedBox(),
+        ],
+      ),
+    );
+  }
+}
